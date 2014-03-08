abf3727f21d5418695619de1c51cc1cb1e25035f
Andrius Janauskas <andrius.janauskas@gmail.com>
2014-03-08 17:19:18 +0200

    Initialize rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OvApp::Application.config.secret_key_base = '7383b6a81bce4121826960681a1d50b764c737ae9ac04afd8eb119c21daed9cee6cb0bde736e38a21b241224f71e56de0bde2a1dabe77aeb3e28cf7fa81ced00'
